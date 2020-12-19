eed12abfda1f10330e830e7cf18bf7585c491e4d
Steve Giordano <stephenagiordano@gmail.com>
2020-12-19 18:05:20 -0500

    Data and React updates

--- react-ftb/github-finder/src/App.js
+++ react-ftb/github-finder/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import './App.css'
 
 class App extends Component {
@@ -10,26 +11,42 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  // async componentDidMount() {
     
-    this.setState({ loading: true })
+  //   this.setState({ loading: true })
 
-    const response = await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //   const response = await fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //   const data = await response.json()
+  //   console.log(data)
+
+  //   this.setState({
+  //     users: data,
+  //     loading: false
+  //   })
+
+  // }
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+    const response = await fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     const data = await response.json()
-    console.log(data)
+    console.log(text, data.items)
 
     this.setState({
-      users: data,
+      users: data.items,
       loading: false
     })
-
+    
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false })
 
   render() {
     return (
       <nav className="App">
         <Navbar/>
         <div className="container">
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false}/>
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </nav>
